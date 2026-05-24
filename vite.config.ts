434c3e74f840dd11a0e4eb4e85a8e620efb8a9f8
user <ito.ken.law@gmail.com>
2026-05-25 00:08:19 +0900

    feat: 失敗のほう

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Vueの場合は@vitejs/plugin-vue
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'FocusWeeks',
+        short_name: 'FocusWeeks',
+        description: 'マイFocusWeeksアプリケーション',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'FocusWeeksicon192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'FocusWeeksicon512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
