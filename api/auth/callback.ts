37449d5b9884abf5b2183da12f2944a027820822
user <ito.ken.law@gmail.com>
2026-05-31 14:00:09 +0900

    feat: 週

--- api/auth/callback.ts
+++ api/auth/callback.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Redis } from '@upstash/redis';
 const kv = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
